9cccfc886418db7f95a395e8f5128845ab62cd4d
Sergey Chaban <loriston@rambler.ru>
2021-11-18 23:28:04 +0300

    Добавлено 2 ДЗ

---
+++ task2.sql
@@ -0,0 +1,21 @@
+create table if not exists list_worker(
+id serial primary key,
+name varchar(30) not null
+);
+
+create table if not exists list_chief(
+id serial primary key,
+id_worker integer references list_worker(id)
+);
+
+create table if not exists list_department(
+id serial primary key,
+title varchar(30) unique,
+id_chief integer references list_chief(id)
+);
+
+create table if not exists list_worker_department(
+id serial primary key,
+id_worker integer not null references list_worker(id),
+id_department integer not null references list_department(id)
+);
